743bb44c9a1377dc821577aa84e2e0a54f2e0e08
clarecorr <clare.e.corrigan@gmail.com>
2017-07-10 10:28:04 -0400

    created first method

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -10,6 +10,32 @@ namespace MethodsPractice
     {
         static void Main(string[] args)
         {
+
+            //Methods are a tool for us to create reusable pieces of code
+            //Methods are a way of us collecting a series of instructions and then calling them when we need them
+            //Methods are always part of a class. Methods are always children of classes
+            //This meand that a method will never be created inside another method or member of the class 
+            //However methods are frequently called withing another method or class
+
+            int answer = Add(5, 7);
+            Console.WriteLine(answer);
+
         }
+
+        //Method Header
+        //Access Modifier - Return Type - Method Name (in Pascal Case (first letter of each word is cap)) (sometimes with parameters)
+        //Static keyword means we don't have to worry about objects
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            //Method Body
+            //A complete method - header and body - is called a method declaration
+
+            int sum = firstNumber + secondNumber;
+
+            return sum;
+            //The return keyword takes whatever balue is determined by usting this method and sends it back to where I call my method
+        }
+        //If you have a method that does not have a return value, you can use keyword void
+        //When we have a void return type, we don't need to use the keyword return
     }
 }
